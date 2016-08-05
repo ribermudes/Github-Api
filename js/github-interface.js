348e86310b6b32be51eda3569dc2e2734aa38f04
Rachael Bermudes <bermudesorma@gmail.com>
2016-08-05 10:56:26 -0700

    working on callback function

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,9 @@
-var Github = require('./../js/github.js').bikeModule;
+var getRepos = require('./../js/github.js').githubModule;
+
+$(document).ready(function() {
+  var inputUserObject = new getRepos();
+  $('#inputUser').click(function() {
+    var userName = $('#inputUser').val();
+    inputUserObject.getUserInfo(userName);
+    });
+  });
--- js/github.js
+++ js/github.js
@@ -1,11 +1,29 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+var apiKey = require('./../.env').apiKey;
+
+function Github() {
+
+}
+
+Github.prototype.getUserInfo = function(username) {
+  $.get('https://api.github.com/users/' + username + '?access_token=OAUTH-TOKEN' + apiKey).then(function(response) {
+    $('#showresults').text("Here is a list of" + name + "'s Repositories. Included is a title and a brief description:");
+          $('#public-repos').append('<li>'+ response[1].name + '</li>' + '<li>' response[1].description + '<li>');
+        });
+      });.fail(function(error){
+      $('#showResults').text(error.responseJSON.message);
+      });
 
 
+
+
+
+
+// .getRepos = function(){
+//   $.get( + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
 
 exports.githubModule = Github;
